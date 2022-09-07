9e4209ee6025f19053cf5daa0e126a5f36ed60f4
ChrissHickss <chrismhicks17@gmail.com>
2022-09-06 22:31:51 -0500

    static files

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,13 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
 
 from django.core.wsgi import get_wsgi_application
+
+
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
-
-application = WhiteNoise(application)
+application = WhiteNoise(application, root="/Users/chrishicks/Desktop/portfolio/staticfiles")
+application.add_files("/Users/chrishicks/Desktop/portfolio/portfolio/static/css")
